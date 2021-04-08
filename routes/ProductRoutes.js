49f4ab962fb5c3caf035177f35f28494458a81e4
Himanshu Sanecha <52252631+himanshusanecha@users.noreply.github.com>
2021-04-08 11:34:51 +0530

    Add files via upload

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -2,15 +2,21 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const ProductController = require('./../controllers/ProductController');
+const AuthController = require('./../controllers/AuthenticationController');
+
 router
     .route('/')
-    .get(ProductController.getAllProducts)
+    .get(AuthController.protect, ProductController.getAllProducts)
     .post(ProductController.CreateProduct);
 
+router
+    .route('/getTourStats')
+    .get(ProductController.getProductStats);
+    
 router
     .route('/:id')
     .get(ProductController.getProduct)
     .patch(ProductController.UpdateProduct)
-    .delete(ProductController.DeleteProduct);
+    .delete(AuthController.protect, AuthController.restrict('admin'), ProductController.DeleteProduct);
 
 module.exports = router;
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,7 +2,21 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const UserController = require('./../controllers/UserController');
+const AuthController = require('./../controllers/AuthenticationController');
+router.post('/signup', AuthController.signup); //this does not follow
+//rest API structure and hence this end point is different 
+//this route is different because here we only want to post the data and not get anything from it
 
+router.post('/login', AuthController.login);
+
+router.post('/forgotpassword', AuthController.forgotPassword); //will recieve the email address as a parameter
+router.patch('/resetpassword/:token', AuthController.resetPassword); //will recieve the token as well as the new password
+
+router.patch('/updatepassword',AuthController.protect, AuthController.updatePassword);
+router.delete('/deleteMe',AuthController.protect, UserController.deleteMe);
+router.patch('/updateMe', AuthController.protect, UserController.updateMe);
+//the below routes and functions are generally used by admin to update the user get all users data 
+//update existing user etc etc
 router
     .route('/')
     .get(UserController.getAllusers)
